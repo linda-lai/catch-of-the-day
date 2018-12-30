e8a26494be53c5b9e4e1b3d387cb73d0b0320ef0
Linda Lai <lindalai.615@gmail.com>
2018-12-30 23:19:41 +1100

    Setup Firebase database/configuration

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -4,6 +4,7 @@ class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
+        <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
       </div>
